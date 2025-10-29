4a3a8c5e366562e1c7f02678397984b39fa815ee
jinghaihan <jhh19980114@gmail.com>
2025-10-29 11:41:31 +0800

    fix: strong convert

--- src/turndown/strong.ts
+++ src/turndown/strong.ts
@@ -4,7 +4,7 @@ export function strong(turndownService: TurndownService) {
   turndownService.addRule('strong', {
     filter: ['strong', 'b'],
     replacement(content) {
-      return `**${content}** `
+      return ` **${content}** `
     },
   })
 }
